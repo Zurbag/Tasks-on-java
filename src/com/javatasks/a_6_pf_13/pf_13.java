a00739effe1c7b651ac0910999d3d5262cca1fd3
Mr.Worker <red21x02@yandex.ru>
2020-01-29 13:05:36 +0300

    pf_13 Put it off until better times

--- src/com/javatasks/a_6_pf_13/pf_13.java
+++ src/com/javatasks/a_6_pf_13/pf_13.java
@@ -1,5 +1,7 @@
 package com.javatasks.a_6_pf_13;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 /*
@@ -12,99 +14,136 @@ public class pf_13 {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
 
-
-        int[] split = split(number, getNumberLength(number));
         int numberLength = getNumberLength(number);
-        switch (numberLength){
-            case 1:
-                System.out.println(convertNumderToString(split[0],numberLength));
-                break;
-            case 2:
-                System.out.println();
-        }
+
+        int[] numberSplit = getNumberSplit(number, numberLength);
+
+        System.out.println("=============");
+        System.out.println(numberLength);
+
+        System.out.println(getNumberWord(numberSplit[0],3));
+        System.out.println(getNumberWord(numberSplit[1],2));
+        System.out.println(getNumberWord(numberSplit[2],1));
+
+
+
     }
 
-    public static int getNumberLength(int number){
+    public static int getNumberLength(int number) {
         int buffer = number;
         int counter = 0;
-        while (buffer > 0){
-            buffer  = buffer/10;
+        while (buffer > 0) {
+            buffer = buffer / 10;
             counter++;
         }
         return counter;
     }
 
-    public static String convertNumderToString(int number, int numberLength){
-        switch (number){
-            case 1:
-                switch (numberLength){
-                    case 1:
-                        return "один";
-                    case 2:
-                        return "десять";
-                    case 3:
-                        return "сто";
-                }
-
-            case 2:
-                switch (numberLength){
-                    case 1:
-                        return "два";
-                    case 2:
-                        return "двадцать";
-                    case 3:
-                        return "двести";
-                }
-            case 3:
-                switch (numberLength){
-                    case 1:
-                        return "три";
-                    case 2:
-                        return "тридцать";
-                    case 3:
-                        return "триста";
-                }
-            case 4:
-                switch (numberLength){
-                    case 1:
-                        return "четыре";
-                    case 2:
-                        return "сорок";
-                    case 3:
-                        return "четыреста";
-                }
-            case 5:
-                switch (numberLength){
-                    case 1:
-                        return "пять";
-                    case 2:
-                        return "пятьдесят";
-                    case 3:
-                        return "пятьсот";
-                }
-            case 6:
-                return "шесть";
-            case 7:
-                return "семь";
-            case 8:
-                return "восемь";
-            case 9:
-                return "девять";
-            case 10:
-                return "десять";
-            default:
-                return "";
-
-        }
-    }
-
-    public static int [] split(int number, int decimal){
-        int [] split = new int[decimal];
+    public static int[] getNumberSplit(int number, int numberLength) {
+        int[] array = new int[numberLength];
         int buffer = number;
-        for (int i = split.length-1; i >= 0; i--) {
-            split[i] = buffer%10;
-            buffer = buffer/10;
+        for (int i = array.length - 1; i >= 0; i--) {
+            array[i] = buffer % 10;
+            buffer = buffer / 10;
         }
-        return split;
+
+        return array;
     }
+
+    public static String [] getNumberWord(int [] number, int numberLength) {
+        for (int i = 0; i < number.length; i++) {
+            switch (number) {
+                case 1:
+
+                    switch (numberLength){
+                        case 1:
+                            return "Один";
+                        case 2:
+                            return "Десять";
+                        case 3:
+                            return "Сто";
+                    }
+                case 2:
+                    switch (numberLength){
+                        case 1:
+                            return "Два";
+                        case 2:
+                            return "Двадцать";
+                        case 3:
+                            return "Двести";
+                    }
+                case 3:
+                    switch (numberLength){
+                        case 1:
+                            return "Три";
+                        case 2:
+                            return "Тридцать";
+                        case 3:
+                            return "Тристо";
+                    }
+                case 4:
+                    switch (numberLength){
+                        case 1:
+                            return "Четыре";
+                        case 2:
+                            return "Сорок";
+                        case 3:
+                            return "Четыре";
+                    }
+                case 5:
+                    switch (numberLength){
+                        case 1:
+                            return "Пять";
+                        case 2:
+                            return "Пятьдесят";
+                        case 3:
+                            return "Пятьсот";
+                    }
+                case 6:
+                    switch (numberLength){
+                        case 1:
+                            return "Шесть";
+                        case 2:
+                            return "Шестьдесят";
+                        case 3:
+                            return "Шестьслот";
+                    }
+                case 7:
+                    switch (numberLength){
+                        case 1:
+                            return "Семь";
+                        case 2:
+                            return "Семьдесят";
+                        case 3:
+                            return "Семьсот";
+                    }
+                case 8:
+                    switch (numberLength){
+                        case 1:
+                            return "Восемь";
+                        case 2:
+                            return "Восемьдесят";
+                        case 3:
+                            return "Восемьсот";
+                    }
+                case 9:
+                    switch (numberLength){
+                        case 1:
+                            return "Девять";
+                        case 2:
+                            return "Девяносто";
+                        case 3:
+                            return "Девятьсот";
+                    }
+
+                default:
+                    return "Введите друго число";
+            }
+
+        }
+
+
+    }
+
+
 }
