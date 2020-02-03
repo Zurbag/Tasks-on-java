19601ca05f70fa96236260e99f89ef1b1a61a5aa
Mr.Worker <red21x02@yandex.ru>
2020-02-03 15:06:27 +0300

    added folder white_rabbit and lesson_5,lessot_6

---
+++ src/com/javatasks/whire_rabbit/lesson_5/Main.java
@@ -0,0 +1,7 @@
+package com.javatasks.whire_rabbit.lesson_5;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hot Java");
+    }
+}
---
+++ src/com/javatasks/whire_rabbit/lesson_6/Numbers.java
@@ -0,0 +1,24 @@
+package com.javatasks.whire_rabbit.lesson_6;
+
+public class Numbers {
+    static byte a = 126;
+    static short b = 777;
+    static int c = 35_000;
+    static long d = 1_000_000_000_000_000L;
+    static char e = 'C';
+    static float f = 7.1f;
+    static double g = 6.3;
+    static boolean h = true;
+    public static void main(String[] args) {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+
+
+    }
+}
