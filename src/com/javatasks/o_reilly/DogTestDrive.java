a8e4429cb87c1df67b5261c135f4af2558836f78
Mr.Kitchen <red21x01@gmail.com>
2020-02-05 21:13:00 +0300

    added DogTest and DogTestDrive

---
+++ src/com/javatasks/o_reilly/DogTestDrive.java
@@ -0,0 +1,9 @@
+package com.javatasks.o_reilly;
+
+public class DogTestDrive {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.size = 40;
+        dog.bark();
+    }
+}
