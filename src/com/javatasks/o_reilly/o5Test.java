ff8ad0585a79d286b1a46f59252d94a2ce60f1a0
Mr.Kitchen <red21x01@gmail.com>
2020-02-05 19:55:45 +0300

    added o5Test

---
+++ src/com/javatasks/o_reilly/o5Test.java
@@ -0,0 +1,14 @@
+package com.javatasks.o_reilly;
+
+public class o5Test {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 0;
+        while (x < 5) {
+            x = x + 1;
+            y+=x;
+            System.out.print(x + "" + y + " ");
+            x = x + 1;
+        }
+    }
+}
