af378fbae691e3670a428370d2af8a5cdf65b1b4
Mr.Kitchen <red21x01@gmail.com>
2020-02-15 22:12:02 +0300

    added Tsa 11, this is a very interesting task

--- src/com/javatasks/a_7_tsa_22/Tsa_11.java
+++ src/com/javatasks/a_7_tsa_22/Tsa_11.java
@@ -1,48 +1,54 @@
 package com.javatasks.a_7_tsa_22;
+/*
+Из цифр числа составить наибольшее число. Ответ получить не
+как набор последовательных цифр, а как настоящее число.
+ */
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Tsa_11 {
     public static void main(String[] args) {
-        int num = 123;
-        int numberLength = getNumsLength(num);
-        int[] nums = getNumsArray(num, numberLength);
-        System.out.println(Arrays.toString(nums));
-        System.out.println(getNumbersOfOptions(num,numberLength));
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int number = scanner.nextInt();
+        int length = getLengthOfNumber(number);
+        int[] numbersSort = getSortedArrayFromNumber(number, length);
+        int maxNumber = getNumberFromArray(numbersSort, length);
+        System.out.println("Максимальная комбинация цифр: " + maxNumber);
 
     }
 
-    public static int getNumbersOfOptions(int num, int numberLength){
-        int n = numberLength;
-        int x = 0;
-        int result = 1;
-        while (x < numberLength){
-            result = result*(n - x);
-            x++;
+    public static int getLengthOfNumber(int number) {
+        int count = 0;
+        while (number > 0) {
+            number /= 10;
+            count++;
         }
-
-        return result;
+        return count;
     }
 
-    public static int getNumsLength(int num) {
-        int length = 0;
-        while (num > 0) {
-            num /= 10;
-            length++;
+    public static int[] getSortedArrayFromNumber(int number, int length) {
+        int[] array = new int[length];
+        for (int i = length - 1; i >= 0; i--) {
+            array[i] = number % 10;
+            number /= 10;
         }
-        return length;
-    }
-
-    public static int[] getNumsArray(int num, int numberLength) {
-        int[] nums = new int[numberLength];
-        for (int i = numberLength - 1; i >= 0; i--) {
-            nums[i] = num % 10;
-            num /= 10;
+        Arrays.sort(array);
+        int[] max = new int[length];
+        for (int i = 0; i < length; i++) {
+            max[i] = array[length - 1 - i];
         }
-        return nums;
+        return max;
     }
 
-
+    public static int getNumberFromArray(int[] numbersSort, int length) {
+        int max = 0;
+        for (int i = 0; i < numbersSort.length; i++) {
+            max = max + numbersSort[i] * (int) Math.pow(10, length - 1 - i);
+        }
+        return max;
+    }
 }
+
+
